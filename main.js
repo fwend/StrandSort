cc7fdd1fa75a5318fc50e0d97a8aa4f43daa6c1a
Jos van Uden <josvanuden@gmail.com>
2019-02-27 21:17:04 +0100

    formatting

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ const arr = [6,4,2,8,1,3,5,7,9];
 const strandSort = (A) => {  
     let result = [];
 
-	while (A.length) {
-		let sorted = [];
-		let leftover = [];
+    while (A.length) {
+        let sorted = [];
+        let leftover = [];
 
-		let lastAdded = A[0];
-		sorted.push(lastAdded);
+        let lastAdded = A[0];
+        sorted.push(lastAdded);
 
-		for (let i = 1; i < A.length; i++) {
+        for (let i = 1; i < A.length; i++) {
             const next = A[i];
             if (next > lastAdded) {
                 sorted.push(next);
-			    lastAdded = next;
+                lastAdded = next;
             } else {
                 leftover.push(next);
-			}
-		}
+            }
+        }
         result = merge(sorted, result);
         A = leftover;        
     }
